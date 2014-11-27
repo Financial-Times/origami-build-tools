febc3e12531982c93153e74eaf1915a00af53a30
Alberto Elias <me@alberto-elias.com>
2014-11-27 09:51:42 +0000

    Start adding tests

---
+++ test/helpers/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var commandLine = require('/../../lib/helpers/command-line');
+
+describe('Command line helper', function() {
+	it('should return output from stdout', function(done) {
+		commandLine.run('echo', ['test']).then(function(output) {
+			expect(output.stdout).to.be('hi');
+		});
+	});
+});
