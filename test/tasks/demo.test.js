5cfb153856db048d12574460f080029430be38b5
Alberto Elias <me@alberto-elias.com>
2014-12-15 13:50:15 +0000

    Started working on demo tests

--- test/tasks/demo.test.js
+++ test/tasks/demo.test.js
@@ -11,21 +11,42 @@ var oTestPath = 'test/fixtures/o-test';
 
 describe('Demo task', function() {
 
+	before(function() {
+		process.chdir(oTestPath)
+	});
+
+	after(function() {
+		process.chdir('../../..');
+	});
+
 	describe('Run server', function() {
 		it('should run a server', function(done) {
 			demo.runServer(gulp)
 				.then(function(server) {
 					server.on('end', function() {
-						console.log('Done');
 						done();
 					});
-				})
+				});
 		});
 	});
 
 	describe('Build demos', function() {
-		it('should fail if ', function(done) {
+		xit('should fail if there is not a config file', function() {
+			process.chdir('../../..');
+			expect(function() {
+				demo(gulp);
+			}).to.throwException(/Couldn't find demos config path, checked: demos\/src\/config.json,demos\/src\/config.js/);
+			process.chdir(oTestPath);
+		});
 
+		xit('should not error with a custom config file', function() {
+			//fs.renameSync('demos/src/config.json', 'demos/src/mysupercoolconfig.json');
+			expect(function() {
+				demo(gulp, {
+					//demoConfig: 'demos/src/mysupercoolconfig.json'
+				});
+			}).to.throwError();
+			//fs.renameSync('demos/src/mysupercoolconfig.json', 'demos/src/config.json');
 		});
 	});
 });
