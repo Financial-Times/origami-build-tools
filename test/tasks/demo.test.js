9576e7b157e507d8e6a0d4b2083cc613f93e267a
Alberto Elias <me@alberto-elias.com>
2015-11-09 15:21:45 +0000

    Addresses issues mentioned by @triblondon

--- test/tasks/demo.test.js
+++ test/tasks/demo.test.js
@@ -14,7 +14,7 @@ const oTestPath = 'test/fixtures/o-test';
 const pathSuffix = '-demo';
 const demoTestPath = path.resolve(obtPath, oTestPath + pathSuffix);
 
-describe('Demo task', () => {
+describe('Demo task', function() {
 
 	before(function() {
 		fs.copySync(path.resolve(obtPath, oTestPath), demoTestPath);
@@ -26,23 +26,23 @@ describe('Demo task', () => {
 		fs.removeSync(demoTestPath);
 	});
 
-	describe('Run server', () => {
-		it('should run a server', (done) => {
+	describe('Run server', function() {
+		it('should run a server', function(done) {
 			demo.runServer(gulp)
 				.then(function(server) {
-					server.on('end', () => {
+					server.on('end', function() {
 						done();
 					});
 				});
 		});
 	});
 
-	describe('Build demos', () => {
-		it('should fail if there is not a config file', (done) => {
+	describe('Build demos', function() {
+		it('should fail if there is not a config file', function(done) {
 			process.chdir(obtPath);
 			fs.writeFileSync('bower.json', '{"name":"o-test"}', 'utf8');
 			demo(gulp)
-				.on('error', (err) => {
+				.on('error', function(err) {
 					expect(err.message).to.be('Couldn\'t find demos config path, checked: demos/src/config.json,demos/src/config.js,origami.json');
 					fs.unlink(path.resolve(obtPath, 'bower.json'));
 					process.chdir(demoTestPath);
@@ -50,7 +50,7 @@ describe('Demo task', () => {
 				});
 		});
 
-		it('should not error with a custom config file', (done) => {
+		it('should not error with a custom config file', function(done) {
 			fs.writeFileSync('bower.json', '{"name":"o-test"}', 'utf8');
 			fs.copySync('demos/src/config.json', 'demos/src/mysupercoolconfig.json');
 			const demoStream = demo(gulp, {
@@ -65,7 +65,7 @@ describe('Demo task', () => {
 			});
 		});
 
-		it('should not fail if there is a config.json file', (done) => {
+		it('should not fail if there is a config.json file', function(done) {
 			const demoStream = demo(gulp)
 				.on('error', function errorHandler(err) {
 						// It will throw a template not found error which is fixed in "should build html" test
@@ -75,7 +75,7 @@ describe('Demo task', () => {
 					});
 		});
 
-		it('should not fail if there is a config.js file', (done) => {
+		it('should not fail if there is a config.js file', function(done) {
 			const config = fs.readFileSync('demos/src/config.json');
 			fs.writeFileSync('demos/src/config.js', 'module.exports = ' + config, 'utf8');
 			const demoStream = demo(gulp)
@@ -88,7 +88,7 @@ describe('Demo task', () => {
 					});
 		});
 
-		it('should not fail using origami.json', (done) => {
+		it('should not fail using origami.json', function(done) {
 			const demoStream = demo(gulp, {
 				demoConfig: 'origami.json'
 			})
@@ -100,7 +100,7 @@ describe('Demo task', () => {
 			})
 		});
 
-		it('should not fail if it\'s using the old config format', (done) => {
+		it('should not fail if it\'s using the old config format', function(done) {
 			const demoStream = demo(gulp, {
 				demoConfig: 'demos/src/oldconfig.json'
 			})
@@ -111,7 +111,7 @@ describe('Demo task', () => {
 			});
 		});
 
-		it('should fail if there are demos with the same name', (done) => {
+		it('should fail if there are demos with the same name', function(done) {
 			const demoConfig = JSON.parse(fs.readFileSync('demos/src/config.json', 'utf8'));
 			demoConfig.demos[1].name = 'test1';
 			fs.writeFileSync('demos/src/config2.json', JSON.stringify(demoConfig));
@@ -126,11 +126,11 @@ describe('Demo task', () => {
 			});
 		});
 
-		it('should build demo html', (done) => {
+		it('should build demo html', function(done) {
 			fs.writeFileSync('demos/src/test1.mustache', '<div>test1</div>', 'utf8');
 			fs.writeFileSync('demos/src/test2.mustache', '<div>test2</div>', 'utf8');
 			const demoStream = demo(gulp)
-			.on('end', () => {
+			.on('end', function() {
 					const test1 = fs.readFileSync('demos/test1.html', 'utf8');
 					const test2 = fs.readFileSync('demos/test2.html', 'utf8');
 					expect(test1).to.contain('<div>test1</div>');
@@ -145,11 +145,11 @@ describe('Demo task', () => {
 			demoStream.resume();
 		});
 
-		it('should build local demos', (done) => {
+		it('should build local demos', function(done) {
 			const demoStream = demo(gulp, {
 				local: true
 			})
-			.on('end', () => {
+			.on('end', function() {
 				expect(fs.readFileSync('demos/local/test1.html', 'utf8')).to.contain('<div>test1</div>');
 				expect(fs.readFileSync('demos/local/test2.html', 'utf8')).to.contain('<div>test2</div>');
 				expect(fs.readFileSync('demos/local/demo.js', 'utf8')).to.contain('function Test() {\n\t\tvar name = \'test\';');
