1dd69c7a19e1e514deecaec51724fc538568d209
Jake Champion <jakechampion.jake2@googlemail.com>
2017-02-21 13:35:54 +0000

    format

--- test/tasks/build.test.js
+++ test/tasks/build.test.js
@@ -2,7 +2,9 @@
 'use strict';
 
 const denodeify = require('denodeify');
-const exec = denodeify(require('child_process').exec, function(err, stdout) { return [err, stdout]; });
+const exec = denodeify(require('child_process').exec, function (err, stdout) {
+	return [err, stdout];
+});
 
 const expect = require('expect.js');
 const gulp = require('gulp');
@@ -15,36 +17,34 @@ const build = require('../../lib/tasks/build');
 const obtPath = process.cwd();
 const oTestPath = 'test/fixtures/o-test';
 
-describe('Build task', function() {
-	describe('Build Js', function() {
+describe('Build task', function () {
+	describe('Build Js', function () {
 		const pathSuffix = '-build-js';
 		const buildTestPath = path.resolve(obtPath, oTestPath + pathSuffix);
 
-		before(function() {
+		before(function () {
 			fs.copySync(path.resolve(obtPath, oTestPath), buildTestPath);
 			process.chdir(buildTestPath);
-			fs.writeFileSync('bower.json', JSON.stringify(
-				{
-					name: 'o-test',
-					main: 'main.js'
-				}
-			), 'utf8');
+			fs.writeFileSync('bower.json', JSON.stringify({
+				name: 'o-test',
+				main: 'main.js'
+			}), 'utf8');
 		});
 
-		after(function() {
+		after(function () {
 			process.chdir(obtPath);
 			fs.removeSync(buildTestPath);
 		});
 
-		afterEach(function() {
-			return fs.emptydirSync('build', function(){
+		afterEach(function () {
+			return fs.emptydirSync('build', function () {
 				fs.removeSync('build');
 			});
 		});
 
-		it('should work with default options', function(done) {
+		it('should work with default options', function (done) {
 			build.js(gulp)
-				.on('end', function() {
+				.on('end', function () {
 					const builtJs = fs.readFileSync('build/main.js', 'utf8');
 					expect(builtJs).to.contain('sourceMappingURL');
 					expect(builtJs).to.contain('var Test');
@@ -55,12 +55,12 @@ describe('Build task', function() {
 				});
 		});
 
-		it('should work with production option', function(done) {
+		it('should work with production option', function (done) {
 			build
 				.js(gulp, {
 					env: 'production'
 				})
-				.on('end', function() {
+				.on('end', function () {
 					const builtJs = fs.readFileSync('build/main.js', 'utf8');
 					expect(builtJs).to.not.contain('sourceMappingURL');
 					expect(builtJs).to.not.contain('var Test');
@@ -68,15 +68,15 @@ describe('Build task', function() {
 					expect(builtJs).to.not.contain('"This is a test"');
 					expect(builtJs).to.not.contain('function Test() {\n\t\tvar name = \'test\';');
 					done();
-			});
+				});
 		});
 
-		it('should build from custom source', function(done) {
+		it('should build from custom source', function (done) {
 			build
 				.js(gulp, {
 					js: './src/js/test.js'
 				})
-				.on('end', function() {
+				.on('end', function () {
 					const builtJs = fs.readFileSync('build/main.js', 'utf8');
 					expect(builtJs).to.contain('sourceMappingURL');
 					expect(builtJs).to.not.contain('var Test');
@@ -85,12 +85,12 @@ describe('Build task', function() {
 				});
 		});
 
-		it('should build to a custom directory', function(done) {
+		it('should build to a custom directory', function (done) {
 			build
 				.js(gulp, {
 					buildFolder: 'test-build'
 				})
-				.on('end', function() {
+				.on('end', function () {
 					const builtJs = fs.readFileSync('test-build/main.js', 'utf8');
 					expect(builtJs).to.contain('sourceMappingURL');
 					expect(builtJs).to.contain('var Test');
@@ -101,12 +101,12 @@ describe('Build task', function() {
 				});
 		});
 
-		it('should build to a custom file', function(done) {
+		it('should build to a custom file', function (done) {
 			build
 				.js(gulp, {
 					buildJs: 'bundle.js'
 				})
-				.on('end', function() {
+				.on('end', function () {
 					const builtJs = fs.readFileSync('build/bundle.js', 'utf8');
 					expect(builtJs).to.contain('sourceMappingURL');
 					expect(builtJs).to.contain('var Test');
@@ -117,45 +117,45 @@ describe('Build task', function() {
 				});
 		});
 
-		it('should fail on syntax error', function(done) {
+		it('should fail on syntax error', function (done) {
 			build
 				.js(gulp, {
 					js: './src/js/syntax-error.js'
 				})
-				.on('error', function(e) {
+				.on('error', function (e) {
 					expect(e.message).to.contain('SyntaxError');
 					expect(e.message).to.contain('Unexpected token');
 					done();
 				})
-				.on('end', function() {
+				.on('end', function () {
 					// Fail quickly to not wait for test to timeout
 					expect(true).to.be(false);
 					done();
 				});
 		});
 
-		it('should fail when a dependency is not found', function(done) {
+		it('should fail when a dependency is not found', function (done) {
 			build
 				.js(gulp, {
 					js: './src/js/missing-dep.js'
 				})
-				.on('error', function(e) {
+				.on('error', function (e) {
 					expect(e.message).to.contain('ModuleNotFoundError: Module not found: Error: Cannot resolve module \'dep\'');
 					done();
 				})
-				.on('end', function() {
+				.on('end', function () {
 					// Fail quickly to not wait for test to timeout
 					expect(true).to.be(false);
 					done();
 				});
 		});
 
-		it('should support a standalone option which creates a global variable', function(done) {
+		it('should support a standalone option which creates a global variable', function (done) {
 			build
 				.js(gulp, {
 					standalone: 'origami'
 				})
-				.on('end', function() {
+				.on('end', function () {
 					const builtJs = fs.readFileSync('build/main.js', 'utf8');
 					expect(builtJs).to.contain('sourceMappingURL');
 					expect(builtJs).to.contain('var Test');
@@ -168,82 +168,82 @@ describe('Build task', function() {
 		});
 	});
 
-	describe('Build Sass', function() {
+	describe('Build Sass', function () {
 		const pathSuffix = '-build-sass';
 		const buildTestPath = path.resolve(obtPath, oTestPath + pathSuffix);
 
-		before(function() {
+		before(function () {
 			fs.copySync(path.resolve(obtPath, oTestPath), buildTestPath);
 			process.chdir(buildTestPath);
-			fs.writeFileSync('bower.json', JSON.stringify(
-				{
-					name: 'o-test',
-					main: 'main.scss'
-				}
-			), 'utf8');
+			fs.writeFileSync('bower.json', JSON.stringify({
+				name: 'o-test',
+				main: 'main.scss'
+			}), 'utf8');
 		});
 
-		after(function() {
+		after(function () {
 			process.chdir(obtPath);
 			fs.removeSync(path.resolve(obtPath, buildTestPath));
 		});
 
-		afterEach(function() {
+		afterEach(function () {
 			return exec('rm -rf build');
 		});
 
-		it('should work with default options', function(done) {
+		it('should work with default options', function (done) {
 			build.sass(gulp)
-				.on('end', function() {
+				.on('end', function () {
 					const builtCss = fs.readFileSync('build/main.css', 'utf8');
 					expect(builtCss).to.contain('div {\n  color: blue; }\n');
 					done();
 				});
 		});
 
-		it('should work with production option', function(done) {
+		it('should work with production option', function (done) {
 			build
 				.sass(gulp, {
 					env: 'production'
 				})
-				.on('end', function() {
+				.on('end', function () {
 					const builtCss = fs.readFileSync('build/main.css', 'utf8');
 					expect(builtCss).to.be('div{color:#00f}');
 					done();
-			});
+				});
 		});
 
-		it('should build from custom source', function(done) {
+		it('should build from custom source', function (done) {
 			build
 				.sass(gulp, {
 					sass: './src/scss/test.scss'
 				})
-				.on('end', function() {
+				.on('end', function () {
 					const builtCss = fs.readFileSync('build/main.css', 'utf8');
 					expect(builtCss).to.contain('p {\n  color: #000000; }\n');
 					done();
 				});
 		});
 
-		it('should build to a custom directory', function(done) {
+		it('should build to a custom directory', function (done) {
 			build
 				.sass(gulp, {
 					buildFolder: 'test-build'
 				})
-				.on('end', function() {
+				.on('end', function () {
 					const builtCss = fs.readFileSync('test-build/main.css', 'utf8');
 					expect(builtCss).to.contain('div {\n  color: blue; }\n');
 					exec('rm -rf test-build')
-						.then(function() { done(); }, done);
+						.then(function () {
+							done();
+						}, done);
 				});
 		});
 
-		it('should build to a custom file', function(done) {
+		it('should build to a custom file', function (done) {
 			build
 				.sass(gulp, {
 					buildCss: 'bundle.css'
 				})
-				.on('end', function() {
+				.on('end', function () {
 					const builtCss = fs.readFileSync('build/bundle.css', 'utf8');
 					expect(builtCss).to.contain('div {\n  color: blue; }\n');
 					done();
