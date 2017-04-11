5ef93992d03a1168a406a2b479fc5bba7ec0bfd6
Jake Champion <me@jakechampion.name>
2017-04-11 15:27:02 +0100

    simplify command line helper (#428)
    * refactor command line module to pipe outputs back to stdout
    
    * update test for command line helper
    
    * add execa dep

--- test/helpers/command-line.test.js
+++ test/helpers/command-line.test.js
@@ -6,18 +6,16 @@ const expect = require('expect.js');
 const commandLine = require('../../lib/helpers/command-line');
 
 describe('Command line helper', function() {
-	it('should return output from stdout', function(done) {
-		commandLine.run('echo', ['test']).then(function(output) {
-			expect(output.stdout).to.be('test\n');
-			done();
+	it('should return output from stdout', function() {
+		return commandLine.run('echo', ['test']).then(function(output) {
+			expect(output.stdout).to.be('test');
 		});
 	});
 
-	it('should return output from stderr', function(done) {
-		commandLine.run('node', ['error']).then(function() {}, function(output) {
-			expect(output.stderr).to.contain('throw err;\n');
-			expect(output.err).to.be(1);
-			done();
+	it('should return output from stderr', function() {
+		return commandLine.run('node', ['error']).then(function () { }, function (output) {
+			expect(output.stderr).to.contain('throw err;');
+			expect(output.code).to.be(1);
 		});
 	});
 });
