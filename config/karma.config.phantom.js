5dd41d3ba1f00198417a46bcf979ca04bfec706e
Jake Champion <jakechampion.jake2@googlemail.com>
2017-05-27 17:20:50 +0100

    Don't modify the base configuration

--- config/karma.config.phantom.js
+++ config/karma.config.phantom.js
@@ -2,10 +2,14 @@ const karmaBaseConfig = require('./karma.config');
 
 module.exports = function (config) {
 
-	const karmaConfig = Object.assign(karmaBaseConfig, {
-		browsers: ['PhantomJS'],
-		logLevel: config.LOG_DISABLE
-	});
+	const karmaConfig = Object.assign(
+		{},
+		karmaBaseConfig,
+		{
+			browsers: ['PhantomJS'],
+			logLevel: config.LOG_DISABLE
+		}
+	);
 
 	config.set(karmaConfig);
 };
