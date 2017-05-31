b7969dd0e9207ef79f0194453ef51928a72ccea5
Jake Champion <jakechampion.jake2@googlemail.com>
2017-05-31 23:58:35 +0100

    Don't output test command or results to terminal

--- test/helpers/command-line.test.js
+++ test/helpers/command-line.test.js
@@ -1,21 +1,27 @@
 /* eslint-env mocha */
 'use strict';
 
-const expect = require('expect.js');
+const proclaim = require('proclaim');
 
 const commandLine = require('../../lib/helpers/command-line');
 
-describe('Command line helper', function() {
-	it('should return output from stdout', function() {
-		return commandLine.run('echo', ['test']).then(function(output) {
-			expect(output.stdout).to.be('test');
+describe('Command line helper', function () {
+	it('should return output from stdout', function () {
+		return commandLine.run('echo', ['test'], {
+			stdout: false,
+			stderr: false
+		}).then(function (output) {
+			proclaim.equal(output.stdout, 'test');
 		});
 	});
 
-	it('should return output from stderr', function() {
-		return commandLine.run('node', ['error']).catch(function (output) {
-			expect(output.stderr).to.contain('throw err;');
-			expect(output.code).to.be(1);
+	it('should return output from stderr', function () {
+		return commandLine.run('node', ['error'], {
+			stdout: false,
+			stderr: false
+		}).catch(function (output) {
+			proclaim.include(output.stderr, 'throw err;');
+			proclaim.equal(output.code, 1);
 		});
 	});
 });
