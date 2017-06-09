4203ed5dad01d56a30d3a24b849d570a6eb9dcd6
Jake Champion <jakechampion.jake2@googlemail.com>
2017-06-09 16:47:34 +0100

    Extract out bower task from bower install function

---
+++ test/tasks/run-bower-install.test.js
@@ -0,0 +1,71 @@
+/* eslint-env mocha */
+'use strict';
+
+const proclaim = require('proclaim');
+const mockery = require('mockery');
+const sinon = require('sinon');
+
+describe('run-bower-install', function() {
+	let runBowerInstall;
+	let commandLine;
+
+	beforeEach(function() {
+		commandLine = {
+			run: sinon.stub().resolves()
+		};
+
+		mockery.enable({
+			useCleanCache: true,
+			warnOnReplace: false,
+			warnOnUnregistered: false
+		});
+
+		mockery.registerMock('../helpers/command-line', commandLine);
+
+		mockery.registerAllowable('../../lib/helpers/run-bower-install');
+
+		runBowerInstall = require('../../lib/tasks/run-bower-install');
+	});
+
+	afterEach(() => {
+		mockery.resetCache();
+		mockery.deregisterAll();
+		mockery.disable();
+	});
+
+	it('exports a function', function() {
+		proclaim.isFunction(runBowerInstall);
+	});
+
+	it('calls bower install via commandLine module', () => {
+		return runBowerInstall()
+			.then(() => {
+				proclaim.calledOnce(commandLine.run);
+				proclaim.calledWithExactly(commandLine.run, require.resolve('bower/bin/bower'), [
+					'install',
+					'--config.registry.search=http://registry.origami.ft.com',
+					'--config.registry.search=https://bower.herokuapp.com'
+				], undefined);
+			});
+	});
+
+	it('returns a rejected promise if commandLine.run returns a rejected promise', () => {
+		commandLine.run.rejects();
+
+		return runBowerInstall()
+			.then(() => {
+				proclaim.fail();
+			}, () => {
+				proclaim.ok(true);
+			});
+	});
+
+	it('returns a resolved promise if commandLine.run returns a resolved promise', () => {
+		commandLine.run.resolves('done');
+
+		return runBowerInstall()
+			.then(result => {
+				proclaim.equal(result, 'done');
+			});
+	});
+});
