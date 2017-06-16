58fdfed4d3d723d89ce1635a9a20d027b2b01c20
Jake Champion <jakechampion.jake2@googlemail.com>
2017-06-16 15:14:01 +0100

    ignore fixtures and add jsdoc to helper function

--- test/integration/helpers/fileExists.js
+++ test/integration/helpers/fileExists.js
@@ -7,6 +7,8 @@ const open = denodeify(require('fs-extra').open);
  * Node.JS no longer has an fs.exists method.
  * Instead we use the fs.open method in read (`r`) mode.
  * fs.open will throw an error if the file does not exist.
+ * @param {string} file file-system path to the file you are wanting to check exists or not
+ * @returns {Promise.<boolean>} Whether the file exists
 */
 module.exports = function fileExists(file) {
 	return open(file, 'r')
