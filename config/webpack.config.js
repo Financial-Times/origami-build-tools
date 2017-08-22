3faac2a93eda15f5c107af1730e0a81629c811d8
Jake Champion <me@jakechampion.name>
2017-08-22 16:57:29 +0100

    Use official bower-resolve-webpack-plugin to have full bower functionality for webpack'd JS (#478)
    * Add complete bower support
    
    * add missing deps to package lock
    
    * Need to resolve directories to `main` because of legacy components

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BowerResolvePlugin = require('bower-resolve-webpack-plugin');
 
 module.exports = {
 	// Fail out on the first error instead of tolerating it.
@@ -14,27 +15,21 @@ module.exports = {
 	cache: false,
 
 	resolve: {
+		// This will handle a bower.json's `main` property being an array.
+		plugins: [new BowerResolvePlugin()],
 		// In which folders the resolver look for modules
 		// relative paths are looked up in every parent folder (like node_modules)
 		// absolute paths are looked up directly
 		// the order is respected
-		modules: ['node_modules', 'bower_components'],
-
+		modules: ['bower_components', 'node_modules'],
 		// These JSON files are read in directories
-		descriptionFiles: ['package.json', 'bower.json'],
-
+		descriptionFiles: ['bower.json', 'package.json'],
 		// These fields in the description files are looked up when trying to resolve the package directory
-		mainFields: ['main', 'browser'],
-
+		mainFields: ['browser', 'main'],
 		// These files are tried when trying to resolve a directory
 		mainFiles: ['index', 'main'],
-
-		// These fields in the description files offer aliasing in this package
-		// The content of these fields is an object where requests to a key are mapped to the corresponding value
-		aliasFields: ['browser'],
-
 		// These extensions are tried when resolving a file
-		extensions: ['.js', '.json'],
+		extensions: ['.js', '.json']
 	},
 	// Resolve loaders (webpack plugins) from the
 	// directory of `OBT` itself rather than the project directory.
