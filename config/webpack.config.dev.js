96bcd75c2ecebe8196daabd2b4a41273449b1e79
Jake Champion <me@jakechampion.name>
2017-09-29 17:03:03 +0100

    Resolve proclaim and old sinon package name to versions under obt's node_modules directory (#482)

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -4,7 +4,9 @@ const webpackMerge = require('webpack-merge');
 const devConfig = {
 	plugins: [
 		new webpack.NamedModulesPlugin(),
-		new webpack.NormalModuleReplacementPlugin(/^sinon$/,require.resolve('sinon'))
+		new webpack.NormalModuleReplacementPlugin(/^sinon$/, require.resolve('sinon')),
+		new webpack.NormalModuleReplacementPlugin(/^sinon\/pkg\/sinon$/, require.resolve('sinon')),
+		new webpack.NormalModuleReplacementPlugin(/^proclaim$/,require.resolve('proclaim'))
 	],
 	devtool: 'inline-source-map'
 };
