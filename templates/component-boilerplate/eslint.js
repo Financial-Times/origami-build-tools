ac321fcbe9116e6c7e928a4194323bca8391f501
Gabrielle von Koss <gabivk@gmail.com>
2018-08-02 14:25:53 +0100

    update .eslint

--- templates/component-boilerplate/eslint.js
+++ templates/component-boilerplate/eslint.js
@@ -2,10 +2,6 @@
 
 module.exports = () => {
 	return `{
-	"extends": "eslint-config-origami-components",
-	"rules": {
-		// Override any settings from the "parent" configuration
-		// For example, Next may want to override some rules for their Next components
-	}
+	"extends": "eslint-config-origami-components"
 }`;
 };
