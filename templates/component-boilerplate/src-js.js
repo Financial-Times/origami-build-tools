2390511cc643167ba553a333381081c9cb3eb1fd
Rob Squires <rob.squires@ft.com>
2018-11-22 14:03:17 +0000

    fix issues picked up by `obt verify`

--- templates/component-boilerplate/src-js.js
+++ templates/component-boilerplate/src-js.js
@@ -5,7 +5,7 @@ module.exports = (name) => {
 	/**
 	 * Class constructor.
 	 * @param {HTMLElement} [${name.camelCase}El] - The component element in the DOM
-	 * @param {Object} [options={}] - An options object for configuring the component
+	 * @param {Object} [opts={}] - An options object for configuring the component
 	 */
 	constructor (${name.camelCase}El, opts) {
 		this.${name.camelCase}El = ${name.camelCase}El;
@@ -18,6 +18,7 @@ module.exports = (name) => {
 	 * Get the data attributes from the ${name.titleCase}Element. If the message is being set up
 	 * declaratively, this method is used to extract the data attributes from the DOM.
 	 * @param {HTMLElement} ${name.camelCase}El - The component element in the DOM
+	 * @returns {Object} - Data attributes as an object
 	 */
 	static getDataAttributes (${name.camelCase}El) {
 		if (!(${name.camelCase}El instanceof HTMLElement)) {
@@ -48,7 +49,8 @@ module.exports = (name) => {
 	/**
 	 * Initialise message component.
 	 * @param {(HTMLElement|String)} rootElement - The root element to intialise the component in, or a CSS selector for the root element
-	 * @param {Object} [options={}] - An options object for configuring the component
+	 * @param {Object} [opts={}] - An options object for configuring the component
+	 * @returns {(${name.titleCase}|Array<${name.titleCase}>)} - ${name.titleCase} instance(s)
 	 */
 	static init (rootEl, opts) {
 		if (!rootEl) {
