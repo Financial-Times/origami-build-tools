6e41202ce83eb92d25ec1ff2d188aa295b03a830
Kate Beard <asouthernbellein@gmail.com>
2019-02-27 11:53:15 +0000

    Add plainTitleCase, fix plainName functions; update references to plainTitleCase

--- templates/component-boilerplate/main-js.js
+++ templates/component-boilerplate/main-js.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = (name) => {
-	return `import ${name.camelCase} from './src/js/${name.original}';
+	return `import ${name.plainTitleCase} from './src/js/${name.plainName}';
 
 const constructAll = function () {
-	${name.camelCase}.init();
+	${name.plainTitleCase}.init();
 	document.removeEventListener('o.DOMContentLoaded', constructAll);
 };
 
 document.addEventListener('o.DOMContentLoaded', constructAll);
 
-export default ${name.camelCase};`;
+export default ${name.plainTitleCase};`;
 };
