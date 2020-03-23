7980e971ed0be6172be7c3689c7ee282c96bfc8f
Jake Champion <jakechampion.jake2@googlemail.com>
2020-03-23 12:07:59 +0000

    update to latest babel  (#696)
    * update to latest babel
    
    * rm file added by accident
    
    * remove commented out code
    
    * Add back obt b bower dependency test
    
    * Add ie 11 target
    
    * fix assertion in unit test
    
    * Update webpack.config.js

--- test/integration/build/fixtures/js-npm-dependency/main.js
+++ test/integration/build/fixtures/js-npm-dependency/main.js
@@ -1,3 +1,4 @@
 'use strict';
 
-global.world = require('lodash.camelcase')('Foo Bar');
+import camel from 'lodash.camelcase';
+global.world = camel('Foo Bar');
--- test/unit/fixtures/o-test/main.js
+++ test/unit/fixtures/o-test/main.js
@@ -1,5 +1,3 @@
-/*eslint-disable */
-const Test = require('./src/js/test');
-const jsonTest = require('./src/js/json-test.json');
-const textTest = requireText('./src/js/require-text.txt');
-/*eslint-enable */
+import test from './src/js/test';
+test();
+
