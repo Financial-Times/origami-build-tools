a0e38fde0437c5dddb52fd7b253d77ed2c3f71bd
notlee <lee.moody@ft.com>
2020-04-27 15:47:01 +0100

    wip: test for all linting rules

--- test/unit/tasks/verify-sass.test.js
+++ test/unit/tasks/verify-sass.test.js
@@ -3,29 +3,22 @@
 
 const proclaim = require('proclaim');
 const process = require('process');
-const rimraf = require('rimraf');
 
-const fs = require('fs-extra');
 const path = require('path');
 
 const verify = require('../../../lib/tasks/verify-sass');
 
 const obtPath = process.cwd();
-const oTestPath = 'test/unit/fixtures/o-test';
-const pathSuffix = '-verify';
-const verifyTestPath = path.resolve(obtPath, oTestPath + pathSuffix);
+const oTestPath = 'test/unit/fixtures/verify';
+const verifyTestPath = path.resolve(obtPath, oTestPath);
 
 describe('verify-sass', function() {
 	beforeEach(function() {
-		fs.copySync(path.resolve(obtPath, oTestPath), verifyTestPath);
 		process.chdir(verifyTestPath);
-		fs.writeFileSync('src/scss/verify.scss', '$color: #ccc;\n\np {\n  color: $color!important ;\n}\n', 'utf8');
-		fs.writeFileSync('src/js/verify.js', 'const test = \'We live in financial times\';\n');
 	});
 
 	afterEach(function() {
 		process.chdir(obtPath);
-		fs.removeSync(path.resolve(obtPath, verifyTestPath));
 	});
 
 	describe('default title', () => {
@@ -36,14 +29,16 @@ describe('verify-sass', function() {
 
 	describe('skip', () => {
 		it('should return true if the file does not exist', () => {
-			rimraf.sync('**/**.scss');
+			// there is no scss to test in the js folder
+			process.chdir(path.resolve(verifyTestPath, 'src/js'));
 			return verify().skip().then(skip => {
 				proclaim.ok(skip);
 			});
 		});
 
 		it('should return a helpful message if the file does not exist', () => {
-			rimraf.sync('**/**.scss');
+			// there is no scss to test in the js folder
+			process.chdir(path.resolve(verifyTestPath, 'src/js'));
 			return verify().skip().then(skip => {
 				proclaim.equal(skip, 'No Sass files found.');
 			});
@@ -58,20 +53,73 @@ describe('verify-sass', function() {
 
 	describe('task', () => {
 		it('should not error if there are no Sass files', () => {
-			rimraf.sync('**/**.scss');
+			// there is no scss to test in the js folder
+			process.chdir(path.resolve(verifyTestPath, 'src/js'));
 			verify().task();
 		});
 
-		it('should throw error if there are linting violations', function() {
+		it('should throw error where there are linting violations', function() {
+			const expectedErrors = {
+				'border-zero': [
+					'border-zero/invalid.scss:2:10',
+					'border-zero/invalid.scss:6:15'
+				],
+				'brace-style': [
+					'brace-style/invalid.scss:2:1',
+					'brace-style/invalid.scss:8:1',
+					'brace-style/invalid.scss:13:1',
+					'brace-style/invalid.scss:18:1',
+				],
+				'class-name-format': [
+					'class-name-format/invalid.scss:1:2',
+					'class-name-format/invalid.scss:5:2',
+					'class-name-format/invalid.scss:9:2',
+					'class-name-format/invalid.scss:13:2',
+					'class-name-format/invalid.scss:17:2'
+				],
+				'clean-import-paths': [
+					'clean-import-paths/invalid.scss:2:2',
+					'clean-import-paths/invalid.scss:4:2',
+					'clean-import-paths/invalid.scss:6:2',
+				],
+				'empty-line-between-blocks': [], // expect no empty-line-between-blocks errors
+				'extends-before-declarations': [
+					'extends-before-declarations/invalid.scss:7:3'
+				],
+				'extends-before-mixins': [
+					'extends-before-mixins/invalid.scss:11:3'
+				],
+				'final-newline': [
+					'final-newline/invalid.scss:4:44'
+				],
+				'function-name-format': [
+      				'function-name-format/invalid.scss:1:11',
+      				'function-name-format/invalid.scss:5:11',
+				]
+
+			};
 			return verify().task().then(() => {
-				proclaim.ok(false);
+				throw new Error('No linting errors were thrown.');
 			}, error => {
-				proclaim.deepStrictEqual(error.message, 'Failed linting: \n\n' +
-				'./src/scss/verify.scss:1:9 Error - Hex values should use the long-form format - 6 characters (hex-length)\n' +
-				'./src/scss/verify.scss:4:10 Error - Trailing semicolons required (trailing-semicolon)\n' +
-				'./src/scss/verify.scss:4:16, Warning - !important not allowed (no-important)\n' +
-				'./src/scss/verify.scss:4:16 Error - Whitespace required before !important (space-before-bang)\n\n' +
-				'4 linting errors');
+				// Assert no SCSS in a valid.scss file throws a linting error.
+				proclaim.doesNotInclude(
+					error.message,
+					'/valid.scss',
+					`Did not expect any Sass in "*/**/valid.scss" to fail.`
+				);
+				// Assert expected lint errors are thrown from invalid.scss files.
+				for (const [expectedError, files] of Object.entries(expectedErrors)) {
+					files.forEach(file => {
+						const expectedPattern = new RegExp(
+							`${file}[^\n]*${expectedError}`
+						);
+						proclaim.match(
+							error.message,
+							expectedPattern,
+							`Expected a "${expectedError}" error from "${file}".`
+						);
+					});
+				}
 			});
 		});
 	});
