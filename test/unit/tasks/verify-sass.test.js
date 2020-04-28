01f0635152f0cb62c0da69f0c95100b285a5901a
notlee <lee.moody@ft.com>
2020-04-28 13:33:47 +0100

    Add more sass lint tests

--- test/unit/tasks/verify-sass.test.js
+++ test/unit/tasks/verify-sass.test.js
@@ -58,7 +58,7 @@ describe('verify-sass', function () {
 			verify().task();
 		});
 
-		it.only('should throw error where there are linting violations', function () {
+		it('should throw error where there are linting violations', function () {
 			const expectedNotices = [
 				{
 					name: 'border-zero',
@@ -199,10 +199,41 @@ describe('verify-sass', function () {
 					]
 				},
 				{
+					// if a user ignores no-ids,
+					// verify the name format of the ids
+					name: 'id-name-format',
+					type: 'Error',
+					locations: [
+						'id-name-format/invalid.scss:4:2',
+						'id-name-format/invalid.scss:8:2',
+						'id-name-format/invalid.scss:12:2',
+						'id-name-format/invalid.scss:16:2',
+						'id-name-format/invalid.scss:20:2'
+					]
+				},
+				{
 					name: 'no-important',
 					type: 'Warning',
 					locations: [
 						'no-important/invalid.scss:2:43'
+					]
+				},
+				{
+					// if the user disables no-important,
+					// verify there is no extra space `! important`
+					name: 'space-after-bang',
+					type: 'Error',
+					locations: [
+						'space-after-bang/invalid.scss:4:20'
+					]
+				},
+				{
+					// if the user disables no-important,
+					// verify there is a space before the bang `; !important`
+					name: 'space-before-bang',
+					type: 'Error',
+					locations: [
+						'space-before-bang/invalid.scss:4:19'
 					]
 				},
 				{
@@ -211,12 +242,157 @@ describe('verify-sass', function () {
 					locations: [
 						'no-invalid-hex/invalid.scss:1:16'
 					]
-				}
+				},
+				{
+					name: 'no-misspelled-properties',
+					type: 'Warning',
+					locations: [
+						'no-misspelled-properties/invalid.scss:8:3',
+						'no-misspelled-properties/invalid.scss:13:3',
+						'no-misspelled-properties/invalid.scss:13:3'
+					]
+				},
+				{
+					name: 'no-qualifying-elements',
+					type: 'Error',
+					locations: [
+						'no-qualifying-elements/invalid.scss:2:4',
+						'no-qualifying-elements/invalid.scss:7:6'
+					]
+				},
+				{
+					name: 'no-qualifying-elements',
+					type: 'Error',
+					locations: [
+						'no-qualifying-elements/invalid.scss:2:4',
+						'no-qualifying-elements/invalid.scss:7:6'
+					]
+				},
+				{
+					name: 'no-trailing-zero',
+					type: 'Error',
+					locations: [
+						'no-trailing-zero/invalid.scss:2:13',
+						'no-trailing-zero/invalid.scss:6:13',
+						'no-trailing-zero/invalid.scss:10:13'
+					]
+				},
+				{
+					name: 'no-vendor-prefixes',
+					type: 'Warning',
+					locations: [
+						'no-vendor-prefixes/invalid.scss:1:2',
+						'no-vendor-prefixes/invalid.scss:6:5',
+						'no-vendor-prefixes/invalid.scss:9:3',
+						'no-vendor-prefixes/invalid.scss:14:5',
+						'no-vendor-prefixes/invalid.scss:15:5',
+						'no-vendor-prefixes/invalid.scss:19:15',
+						'no-vendor-prefixes/invalid.scss:23:5',
+						'no-vendor-prefixes/invalid.scss:24:5',
+						'no-vendor-prefixes/invalid.scss:25:5',
+						'no-vendor-prefixes/invalid.scss:26:5',
+						'no-vendor-prefixes/invalid.scss:27:5',
+						'no-vendor-prefixes/invalid.scss:28:5'
+					]
+				},
+				{
+					name: 'placeholder-in-extend',
+					type: 'Error',
+					locations: [
+						'placeholder-in-extend/invalid.scss:6:11'
+					]
+				},
+				{
+					name: 'placeholder-name-format',
+					type: 'Error',
+					locations: [
+						'placeholder-name-format/invalid.scss:1:1',
+						'placeholder-name-format/invalid.scss:5:1',
+						'placeholder-name-format/invalid.scss:9:1',
+						'placeholder-name-format/invalid.scss:13:1',
+						'placeholder-name-format/invalid.scss:17:1'
+					]
+				},
+				{
+					name: 'shorthand-values',
+					type: 'Error',
+					locations: [
+						'shorthand-values/invalid.scss:2:13',
+						'shorthand-values/invalid.scss:6:13',
+						'shorthand-values/invalid.scss:10:13'
+					]
+				},
+				{
+					name: 'single-line-per-selector',
+					type: 'Error',
+					locations: [
+						'single-line-per-selector/invalid.scss:1:7'
+					]
+				},
+				{
+					name: 'space-after-colon',
+					type: 'Error',
+					locations: [
+						'space-after-colon/invalid.scss:2:12'
+					]
+				},
+				{
+					name: 'space-after-comma',
+					type: 'Error',
+					locations: [
+						'space-after-comma/invalid.scss:1:25',
+						'space-after-comma/invalid.scss:6:23',
+						'space-after-comma/invalid.scss:7:30'
+					]
+				},
+				{
+					name: 'space-before-brace',
+					type: 'Error',
+					locations: [
+						'space-before-brace/invalid.scss:1:4',
+						'space-before-brace/invalid.scss:2:16',
+						'space-before-brace/invalid.scss:7:10'
+					]
+				},
+				{
+					name: 'space-before-colon',
+					type: 'Error',
+					locations: [
+						'space-before-colon/invalid.scss:3:12'
+					]
+				},
+				{
+					name: 'space-between-parens',
+					type: 'Error',
+					locations: [
+						'space-between-parens/invalid.scss:1:15',
+						'space-between-parens/invalid.scss:1:20',
+						'space-between-parens/invalid.scss:5:16',
+						'space-between-parens/invalid.scss:10:18',
+						'space-between-parens/invalid.scss:10:26',
+						'space-between-parens/invalid.scss:11:18',
+						'space-between-parens/invalid.scss:12:17'
+					]
+				},
+				{
+					name: 'trailing-semicolon',
+					type: 'Error',
+					locations: [
+						'trailing-semicolon/invalid.scss:4:19'
+					]
+				},
+				{
+					name: 'variable-name-format',
+					type: 'Error',
+					locations: [
+						'variable-name-format/invalid.scss:1:1',
+						'variable-name-format/invalid.scss:3:1'
+					]
+				},
 			];
 			return verify().task().then(() => {
 				throw new Error('No linting errors were thrown.');
 			}, error => {
-				console.log(error.message);
 				// Assert no SCSS in a valid.scss file throws a linting error.
 				proclaim.doesNotInclude(
 					error.message,
