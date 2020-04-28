728d811b3d87f6579d35d3316a88ee94d8f2cf04
notlee <lee.moody@ft.com>
2020-04-28 11:56:56 +0100

    wip: test linting notices are errors or warnings as expected

--- test/unit/tasks/verify-sass.test.js
+++ test/unit/tasks/verify-sass.test.js
@@ -12,12 +12,12 @@ const obtPath = process.cwd();
 const oTestPath = 'test/unit/fixtures/verify';
 const verifyTestPath = path.resolve(obtPath, oTestPath);
 
-describe('verify-sass', function() {
-	beforeEach(function() {
+describe('verify-sass', function () {
+	beforeEach(function () {
 		process.chdir(verifyTestPath);
 	});
 
-	afterEach(function() {
+	afterEach(function () {
 		process.chdir(obtPath);
 	});
 
@@ -58,95 +58,165 @@ describe('verify-sass', function() {
 			verify().task();
 		});
 
-		it.only('should throw error where there are linting violations', function() {
-			const expectedErrors = {
-				'border-zero': [
-					'border-zero/invalid.scss:2:10',
-					'border-zero/invalid.scss:6:15'
-				],
-				'brace-style': [
-					'brace-style/invalid.scss:2:1',
-					'brace-style/invalid.scss:8:1',
-					'brace-style/invalid.scss:13:1',
-					'brace-style/invalid.scss:18:1',
-				],
-				'class-name-format': [
-					'class-name-format/invalid.scss:1:2',
-					'class-name-format/invalid.scss:5:2',
-					'class-name-format/invalid.scss:9:2',
-					'class-name-format/invalid.scss:13:2',
-					'class-name-format/invalid.scss:17:2'
-				],
-				'clean-import-paths': [
-					'clean-import-paths/invalid.scss:2:2',
-					'clean-import-paths/invalid.scss:4:2',
-					'clean-import-paths/invalid.scss:6:2',
-				],
-				'empty-line-between-blocks': [], // expect no empty-line-between-blocks errors
-				'extends-before-declarations': [
-					'extends-before-declarations/invalid.scss:7:3'
-				],
-				'extends-before-mixins': [
-					'extends-before-mixins/invalid.scss:11:3'
-				],
-				'final-newline': [
-					'final-newline/invalid.scss:4:44'
-				],
-				'function-name-format': [
-      				'function-name-format/invalid.scss:1:11',
-      				'function-name-format/invalid.scss:5:11',
-				],
-				'hex-length': [
-					'hex-length/invalid.scss:1:13',
-					'hex-length/invalid.scss:4:36',
-					'hex-length/invalid.scss:4:42',
-					'hex-length/invalid.scss:8:10'
-				],
-				'hex-notation': [
-					'hex-notation/invalid.scss:2:13',
-					'hex-notation/invalid.scss:5:36',
-					'hex-notation/invalid.scss:5:45',
-					'hex-notation/invalid.scss:9:10'
-				],
-				'indentation': [], // tabs or spaces, we don't verify
-				'leading-zero': [
-					'leading-zero/invalid.scss:2:13'
-				],
-				'mixin-name-format': [
-					'mixin-name-format/invalid.scss:1:1',
-					'mixin-name-format/invalid.scss:5:1'
-				],
-				'mixins-before-declarations': [
-					'mixins-before-declarations/invalid.scss:9:5'
-				],
-				'nesting-depth': [
-					'nesting-depth/invalid.scss:5:17'
-				],
-				'no-color-keywords': [], // we allow colour keywords
-				'no-color-literals': [], // we allow css colour functions
-				'no-css-comments': [], // we allow css comments
-				'no-debug': [
-					'no-debug/invalid.scss:1:2'
-				],
-				'no-duplicate-properties': [
-					'no-duplicate-properties/invalid.scss:3:3',
-					'no-duplicate-properties/invalid.scss:8:3',
-				],
-				'no-empty-ruleset': [], // we allow empty rulesets
-				'no-ids': [
-					'no-ids/invalid.scss:1:1'
-				],
-				'no-important': [
-					'no-important/invalid.scss:2:43'
-				],
-				'no-invalid-hex': [
-					'no-invalid-hex/invalid.scss:1:16'
-				]
-			};
+		it.only('should throw error where there are linting violations', function () {
+			const expectedNotices = [
+				{
+					name: 'border-zero',
+					type: 'Error',
+					locations: [
+						'border-zero/invalid.scss:2:10',
+						'border-zero/invalid.scss:6:15'
+					]
+				},
+				{
+					name: 'brace-style',
+					type: 'Error',
+					locations: [
+						'brace-style/invalid.scss:2:1',
+						'brace-style/invalid.scss:8:1',
+						'brace-style/invalid.scss:13:1',
+						'brace-style/invalid.scss:18:1',
+					]
+				},
+				{
+					name: 'class-name-format',
+					type: 'Error',
+					locations: [
+						'class-name-format/invalid.scss:1:2',
+						'class-name-format/invalid.scss:5:2',
+						'class-name-format/invalid.scss:9:2',
+						'class-name-format/invalid.scss:13:2',
+						'class-name-format/invalid.scss:17:2'
+					]
+				},
+				{
+					name: 'clean-import-paths',
+					type: 'Warning',
+					locations: [
+						'clean-import-paths/invalid.scss:2:2',
+						'clean-import-paths/invalid.scss:4:2',
+						'clean-import-paths/invalid.scss:6:2',
+					]
+				},
+				{
+					name: 'extends-before-declarations',
+					type: 'Warning',
+					locations: [
+						'extends-before-declarations/invalid.scss:7:3'
+					]
+				},
+				{
+					name: 'extends-before-mixins',
+					type: 'Warning',
+					locations: [
+						'extends-before-mixins/invalid.scss:11:3'
+					]
+				},
+				{
+					name: 'final-newline',
+					type: 'Warning',
+					locations: [
+						'final-newline/invalid.scss:4:44'
+					]
+				},
+				{
+					name: 'function-name-format',
+					type: 'Error',
+					locations: [
+						'function-name-format/invalid.scss:1:11',
+						'function-name-format/invalid.scss:5:11',
+					]
+				},
+				{
+					name: 'hex-length',
+					type: 'Error',
+					locations: [
+						'hex-length/invalid.scss:1:13',
+						'hex-length/invalid.scss:4:36',
+						'hex-length/invalid.scss:4:42',
+						'hex-length/invalid.scss:8:10'
+					]
+				},
+				{
+					name: 'hex-notation',
+					type: 'Warning',
+					locations: [
+						'hex-notation/invalid.scss:2:13',
+						'hex-notation/invalid.scss:5:36',
+						'hex-notation/invalid.scss:5:45',
+						'hex-notation/invalid.scss:9:10'
+					]
+				},
+				{
+					name: 'leading-zero',
+					type: 'Error',
+					locations: [
+						'leading-zero/invalid.scss:2:13'
+					]
+				},
+				{
+					name: 'mixin-name-format',
+					type: 'Error',
+					locations: [
+						'mixin-name-format/invalid.scss:1:1',
+						'mixin-name-format/invalid.scss:5:1'
+					]
+				},
+				{
+					name: 'mixins-before-declarations',
+					type: 'Warning',
+					locations: [
+						'mixins-before-declarations/invalid.scss:9:5'
+					]
+				},
+				{
+					name: 'nesting-depth',
+					type: 'Warning',
+					locations: [
+						'nesting-depth/invalid.scss:5:17'
+					]
+				},
+				{
+					name: 'no-debug',
+					type: 'Error',
+					locations: [
+						'no-debug/invalid.scss:1:2'
+					]
+				},
+				{
+					name: 'no-duplicate-properties',
+					type: 'Warning',
+					locations: [
+						'no-duplicate-properties/invalid.scss:3:3',
+						'no-duplicate-properties/invalid.scss:8:3',
+					]
+				},
+				{
+					name: 'no-ids',
+					type: 'Warning',
+					locations: [
+						'no-ids/invalid.scss:1:1'
+					]
+				},
+				{
+					name: 'no-important',
+					type: 'Warning',
+					locations: [
+						'no-important/invalid.scss:2:43'
+					]
+				},
+				{
+					name: 'no-invalid-hex',
+					type: 'Warning',
+					locations: [
+						'no-invalid-hex/invalid.scss:1:16'
+					]
+				}
+			];
 			return verify().task().then(() => {
 				throw new Error('No linting errors were thrown.');
 			}, error => {
-					console.log(error.message);
+				console.log(error.message);
 				// Assert no SCSS in a valid.scss file throws a linting error.
 				proclaim.doesNotInclude(
 					error.message,
@@ -154,15 +224,15 @@ describe('verify-sass', function() {
 					`Did not expect any Sass in "*/**/valid.scss" to fail.`
 				);
 				// Assert expected lint errors are thrown from invalid.scss files.
-				for (const [expectedError, files] of Object.entries(expectedErrors)) {
-					files.forEach(file => {
+					for (const { name, type, locations } of expectedNotices) {
+					locations.forEach(location => {
 						const expectedPattern = new RegExp(
-							`${file}[^\n]*${expectedError}`
+							`${location}[^\n]*${type}[^\n]*${name}`
 						);
 						proclaim.match(
 							error.message,
 							expectedPattern,
-							`Expected a "${expectedError}" error from "${file}".`
+							`Expected a "${name}" "${type}" from "${location}".`
 						);
 					});
 				}
