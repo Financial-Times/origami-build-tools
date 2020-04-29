e3e73546cf3bd67427b087848be3ec7caa5d61e6
notlee <lee.moody@ft.com>
2020-04-29 17:42:06 +0100

    Correct boilerplate indent to tabs

--- templates/component-boilerplate/src-sass-brand.js
+++ templates/component-boilerplate/src-sass-brand.js
@@ -1,17 +1,16 @@
 'use strict';
 
 module.exports = (name) => {
-	return `
-/// Helper for \`o-brand\` function.
+	return `/// Helper for \`o-brand\` function.
 /// @access private
 @function _${name.camelCase}Get($variables, $from: null) {
-    @return oBrandGet($component: '${name.original}', $variables: $variables, $from: $from);
+	@return oBrandGet($component: '${name.original}', $variables: $variables, $from: $from);
 }
 
 /// Helper for \`o-brand\` function.
 /// @access private
 @function _${name.camelCase}Supports($variant) {
-    @return oBrandSupportsVariant($component: '${name.original}', $variant: $variant);
+	@return oBrandSupportsVariant($component: '${name.original}', $variant: $variant);
 }
 `;
 };
