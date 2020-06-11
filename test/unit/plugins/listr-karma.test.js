fa6ce06b063af4e10e1fdab735bea7f81070591e
Jake Champion <me@jakechampion.name>
2020-06-11 11:26:54 +0100

    add tests for the karma reporter when running in a CI environment

--- test/unit/plugins/listr-karma.test.js
+++ test/unit/plugins/listr-karma.test.js
@@ -16,20 +16,6 @@ describe('SpecReporter', function () {
 	let SpecReporter;
 	let errors;
 
-	beforeEach(function() {
-		mockery.enable({
-			useCleanCache: true,
-			warnOnReplace: false,
-			warnOnUnregistered: false
-		});
-		mockery.registerMock('is-ci', false);
-		mockery.registerAllowable('../../../lib/plugins/listr-karma.js');
-
-		const listrKarmaReporter = require('../../../lib/plugins/listr-karma.js')();
-		SpecReporter = listrKarmaReporter.reporter['reporter:listr'];
-		errors = listrKarmaReporter.errors;
-	});
-
 	afterEach(() => {
 		mockery.resetCache();
 		mockery.deregisterAll();
@@ -37,39 +23,30 @@ describe('SpecReporter', function () {
 	});
 
 	describe('functionality', function () {
-		describe('onRunComplete', function () {
-			describe('with no browsers', function () {
-				let newSpecReporter;
-
-				beforeEach(function () {
-					newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
-
-					newSpecReporter.currentSuite.push('suite name');
-					newSpecReporter.onRunComplete([], []);
+		context('running in a CI environment', function() {
+			beforeEach(function() {
+				mockery.enable({
+					useCleanCache: true,
+					warnOnReplace: false,
+					warnOnUnregistered: false
 				});
+				mockery.registerMock('is-ci', true);
+				mockery.registerAllowable('../../../lib/plugins/listr-karma.js');
 
-				it('should reset failedSpecs object', function () {
-					proclaim.deepEqual(newSpecReporter.failedSpecs, {});
-				});
-
-				it('should reset currentSuite arrays', function () {
-					proclaim.equal(newSpecReporter.currentSuite.length, 0);
-				});
-
-				it('should not add anything to the errors array', function () {
-					proclaim.equal(errors.length, 0);
-				});
+				const listrKarmaReporter = require('../../../lib/plugins/listr-karma.js')();
+				SpecReporter = listrKarmaReporter.reporter['reporter:listr'];
+				errors = listrKarmaReporter.errors;
 			});
 
-			describe('with browsers', function () {
-				describe('and there are no failures', function () {
+			describe('onRunComplete', function () {
+				describe('with no browsers', function () {
 					let newSpecReporter;
 
 					beforeEach(function () {
 						newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
-						newSpecReporter.onRunComplete(['testValue'], {
-							failed: 0
-						});
+
+						newSpecReporter.currentSuite.push('suite name');
+						newSpecReporter.onRunComplete([], []);
 					});
 
 					it('should reset failedSpecs object', function () {
@@ -85,28 +62,161 @@ describe('SpecReporter', function () {
 					});
 				});
 
-				describe('and there are failures', function () {
-					let newSpecReporter;
-					let browser;
-					let failedSpec;
-					beforeEach(function () {
-						browser = {
-							name: 'Test Browser',
-							id: 'testValue'
-						};
-						failedSpec = {
-							suite: ['suite name'],
-							description: 'description of test',
-							log: ['log', 'message']
-						};
-						newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
-						newSpecReporter.currentSuite.push(failedSpec.suite);
-						newSpecReporter.failedSpecs = {
-							[browser.id]: [failedSpec]
-						};
-						newSpecReporter.onRunComplete([browser], {
-							failed: 1
+				describe('with browsers', function () {
+					describe('and there are no failures', function () {
+						let newSpecReporter;
+
+						beforeEach(function () {
+							newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
+							newSpecReporter.onRunComplete(['testValue'], {
+								failed: 0
+							});
 						});
+
+						it('should reset failedSpecs object', function () {
+							proclaim.deepEqual(newSpecReporter.failedSpecs, {});
+						});
+
+						it('should reset currentSuite arrays', function () {
+							proclaim.equal(newSpecReporter.currentSuite.length, 0);
+						});
+
+						it('should not add anything to the errors array', function () {
+							proclaim.equal(errors.length, 0);
+						});
+					});
+
+					describe('and there are failures', function () {
+						let newSpecReporter;
+						let browser;
+						let failedSpec;
+						beforeEach(function () {
+							browser = {
+								name: 'Test Browser',
+								id: 'testValue'
+							};
+							failedSpec = {
+								suite: ['suite name'],
+								description: 'description of test',
+								log: ['log', 'test/add.test.js']
+							};
+							newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
+							newSpecReporter.currentSuite.push(failedSpec.suite);
+							newSpecReporter.failedSpecs = {
+								[browser.id]: [failedSpec]
+							};
+							newSpecReporter.onRunComplete([browser], {
+								failed: 1
+							});
+						});
+
+						it('should reset failedSpecs object', function () {
+							proclaim.deepEqual(newSpecReporter.failedSpecs, {});
+						});
+
+						it('should reset currentSuite arrays', function () {
+							proclaim.equal(newSpecReporter.currentSuite.length, 0);
+						});
+
+						it('should add error message to the errors array', function () {
+							proclaim.deepEqual(errors, [
+								colors.white('Test Browser failed specs:'),
+								colors.white('test/add.test.js') + ' suite name > description of test',
+								colors.white('Test Browser errored with:') + ' ' + colors.red('log'),
+								'1 tests failed across 1 browsers.'
+							]);
+						});
+
+						it('should be idempotent', () => {
+							newSpecReporter.currentSuite.push(failedSpec.suite);
+							newSpecReporter.failedSpecs = {
+								[browser.id]: [failedSpec]
+							};
+							newSpecReporter.onRunComplete([browser], {
+								failed: 1
+							});
+
+							proclaim.deepEqual(errors, [
+								colors.white('Test Browser failed specs:'),
+								colors.white('test/add.test.js') + ' suite name > description of test',
+								colors.white('Test Browser errored with:') + ' ' + colors.red('log'),
+								'1 tests failed across 1 browsers.'
+							]);
+						});
+					});
+				});
+			});
+
+			describe('onSpecComplete', function () {
+				let newSpecReporter;
+				let browser;
+				let failedResult;
+				let passedResult;
+
+				beforeEach(() => {
+					browser = {
+						name: 'Test Browser',
+						id: 'testValue'
+					};
+					failedResult = {
+						suite: ['suite name'],
+						description: 'description of test',
+						log: ['log message'],
+						success : false
+					};
+
+					passedResult = {
+						suite: ['suite name'],
+						description: 'description of test',
+						log: ['log message'],
+						success : true
+					};
+					newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
+				});
+
+				describe('no failed specs', () => {
+					it('should not anything to failedSpecs ', function () {
+						newSpecReporter.onSpecComplete(browser, passedResult);
+						proclaim.deepEqual(newSpecReporter.failedSpecs, {});
+					});
+				});
+
+				describe('failed specs', () => {
+					it('should add array of failed specs for browser to the failedSpecs property ', function () {
+						newSpecReporter.onSpecComplete(browser, failedResult);
+
+						const failedSpec = Object.create(null);
+						failedSpec[browser.id] = [failedResult];
+					});
+				});
+			});
+		});
+
+		context('running in a non-CI environment', function() {
+
+			beforeEach(function() {
+				mockery.enable({
+					useCleanCache: true,
+					warnOnReplace: false,
+					warnOnUnregistered: false
+				});
+				mockery.registerMock('is-ci', false);
+				mockery.registerAllowable('../../../lib/plugins/listr-karma.js');
+
+				const listrKarmaReporter = require('../../../lib/plugins/listr-karma.js')();
+				SpecReporter = listrKarmaReporter.reporter['reporter:listr'];
+				errors = listrKarmaReporter.errors;
+			});
+
+			describe('onRunComplete', function () {
+				describe('with no browsers', function () {
+					let newSpecReporter;
+
+					beforeEach(function () {
+						newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
+
+						newSpecReporter.currentSuite.push('suite name');
+						newSpecReporter.onRunComplete([], []);
 					});
 
 					it('should reset failedSpecs object', function () {
@@ -117,77 +227,139 @@ describe('SpecReporter', function () {
 						proclaim.equal(newSpecReporter.currentSuite.length, 0);
 					});
 
-					it('should add error message to the errors array', function () {
-						proclaim.deepEqual(errors, [
-							colors.white('Test Browser failed specs:'),
-							colors.white('suite name > description of test'),
-							colors.red('log\t'),
-							colors.red('message\t'),
-							'1 tests failed across 1 browsers.'
-						]);
+					it('should not add anything to the errors array', function () {
+						proclaim.equal(errors.length, 0);
 					});
+				});
 
-					it('should be idempotent', () => {
-						newSpecReporter.currentSuite.push(failedSpec.suite);
-						newSpecReporter.failedSpecs = {
-							[browser.id]: [failedSpec]
-						};
-						newSpecReporter.onRunComplete([browser], {
-							failed: 1
+				describe('with browsers', function () {
+					describe('and there are no failures', function () {
+						let newSpecReporter;
+
+						beforeEach(function () {
+							newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
+							newSpecReporter.onRunComplete(['testValue'], {
+								failed: 0
+							});
 						});
 
-						proclaim.deepEqual(errors, [
-							colors.white('Test Browser failed specs:'),
-							colors.white('suite name > description of test'),
-							colors.red('log\t'),
-							colors.red('message\t'),
-							'1 tests failed across 1 browsers.'
-						]);
+						it('should reset failedSpecs object', function () {
+							proclaim.deepEqual(newSpecReporter.failedSpecs, {});
+						});
+
+						it('should reset currentSuite arrays', function () {
+							proclaim.equal(newSpecReporter.currentSuite.length, 0);
+						});
+
+						it('should not add anything to the errors array', function () {
+							proclaim.equal(errors.length, 0);
+						});
+					});
+
+					describe('and there are failures', function () {
+						let newSpecReporter;
+						let browser;
+						let failedSpec;
+						beforeEach(function () {
+							browser = {
+								name: 'Test Browser',
+								id: 'testValue'
+							};
+							failedSpec = {
+								suite: ['suite name'],
+								description: 'description of test',
+								log: ['log', 'test/add.test.js']
+							};
+							newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
+							newSpecReporter.currentSuite.push(failedSpec.suite);
+							newSpecReporter.failedSpecs = {
+								[browser.id]: [failedSpec]
+							};
+							newSpecReporter.onRunComplete([browser], {
+								failed: 1
+							});
+						});
+
+						it('should reset failedSpecs object', function () {
+							proclaim.deepEqual(newSpecReporter.failedSpecs, {});
+						});
+
+						it('should reset currentSuite arrays', function () {
+							proclaim.equal(newSpecReporter.currentSuite.length, 0);
+						});
+
+						it('should add error message to the errors array', function () {
+							proclaim.deepEqual(errors, [
+								colors.white('Test Browser failed specs:'),
+								colors.white('suite name > description of test'),
+								colors.red('log\t'),
+								colors.red('test/add.test.js\t'),
+								'1 tests failed across 1 browsers.'
+							]);
+						});
+
+						it('should be idempotent', () => {
+							newSpecReporter.currentSuite.push(failedSpec.suite);
+							newSpecReporter.failedSpecs = {
+								[browser.id]: [failedSpec]
+							};
+							newSpecReporter.onRunComplete([browser], {
+								failed: 1
+							});
+
+							proclaim.deepEqual(errors, [
+								colors.white('Test Browser failed specs:'),
+								colors.white('suite name > description of test'),
+								colors.red('log\t'),
+								colors.red('test/add.test.js\t'),
+								'1 tests failed across 1 browsers.'
+							]);
+						});
 					});
 				});
 			});
-		});
 
-		describe('onSpecComplete', function () {
-			let newSpecReporter;
-			let browser;
-			let failedResult;
-			let passedResult;
+			describe('onSpecComplete', function () {
+				let newSpecReporter;
+				let browser;
+				let failedResult;
+				let passedResult;
 
-			beforeEach(() => {
-				browser = {
-					name: 'Test Browser',
-					id: 'testValue'
-				};
-				failedResult = {
-					suite: ['suite name'],
-					description: 'description of test',
-					log: ['log message'],
-					success : false
-				};
+				beforeEach(() => {
+					browser = {
+						name: 'Test Browser',
+						id: 'testValue'
+					};
+					failedResult = {
+						suite: ['suite name'],
+						description: 'description of test',
+						log: ['log message'],
+						success : false
+					};
 
-				passedResult = {
-					suite: ['suite name'],
-					description: 'description of test',
-					log: ['log message'],
-					success : true
-				};
-				newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
-			});
-
-			describe('no failed specs', () => {
-				it('should not anything to failedSpecs ', function () {
-					newSpecReporter.onSpecComplete(browser, passedResult);
-					proclaim.deepEqual(newSpecReporter.failedSpecs, {});
+					passedResult = {
+						suite: ['suite name'],
+						description: 'description of test',
+						log: ['log message'],
+						success : true
+					};
+					newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
 				});
-			});
 
-			describe('failed specs', () => {
-				it('should add array of failed specs for browser to the failedSpecs property ', function () {
-					newSpecReporter.onSpecComplete(browser, failedResult);
+				describe('no failed specs', () => {
+					it('should not anything to failedSpecs ', function () {
+						newSpecReporter.onSpecComplete(browser, passedResult);
+						proclaim.deepEqual(newSpecReporter.failedSpecs, {});
+					});
+				});
 
-					const failedSpec = Object.create(null);
-					failedSpec[browser.id] = [failedResult];
+				describe('failed specs', () => {
+					it('should add array of failed specs for browser to the failedSpecs property ', function () {
+						newSpecReporter.onSpecComplete(browser, failedResult);
+
+						const failedSpec = Object.create(null);
+						failedSpec[browser.id] = [failedResult];
+					});
 				});
 			});
 		});
