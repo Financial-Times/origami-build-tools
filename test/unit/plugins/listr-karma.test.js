d1405b048721fbd5b12f628a450c50854cd055d4
Jake Champion <me@jakechampion.name>
2020-06-12 17:06:36 +0100

    Add tests for karma github annotations

--- test/unit/plugins/listr-karma.test.js
+++ test/unit/plugins/listr-karma.test.js
@@ -5,6 +5,11 @@
 const mockery = require('mockery');
 const colors = require('colors/safe');
 const proclaim = require('proclaim');
+const sinon = require('sinon');
+sinon.assert.expose(proclaim, {
+	includeFail: false,
+	prefix: ''
+});
 
 const formatError = function (a, b) {
 	return a + b;
@@ -24,13 +29,18 @@ describe('SpecReporter', function () {
 
 	describe('functionality', function () {
 		context('running in a CI environment', function() {
+			let console;
 			beforeEach(function() {
 				mockery.enable({
 					useCleanCache: true,
 					warnOnReplace: false,
 					warnOnUnregistered: false
 				});
+				console = {
+					log: sinon.stub()
+				};
 				mockery.registerMock('is-ci', true);
+				global.console = console;
 				mockery.registerAllowable('../../../lib/plugins/listr-karma.js');
 
 				const listrKarmaReporter = require('../../../lib/plugins/listr-karma.js')();
@@ -38,30 +48,11 @@ describe('SpecReporter', function () {
 				errors = listrKarmaReporter.errors;
 			});
 
+			afterEach(() => {
+				console.log.reset();
+			});
+
 			describe('onRunComplete', function () {
-				describe('with no browsers', function () {
-					let newSpecReporter;
-
-					beforeEach(function () {
-						newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
-
-						newSpecReporter.currentSuite.push('suite name');
-						newSpecReporter.onRunComplete([], []);
-					});
-
-					it('should reset failedSpecs object', function () {
-						proclaim.deepEqual(newSpecReporter.failedSpecs, {});
-					});
-
-					it('should reset currentSuite arrays', function () {
-						proclaim.equal(newSpecReporter.currentSuite.length, 0);
-					});
-
-					it('should not add anything to the errors array', function () {
-						proclaim.equal(errors.length, 0);
-					});
-				});
-
 				describe('with browsers', function () {
 					describe('and there are no failures', function () {
 						let newSpecReporter;
@@ -73,16 +64,8 @@ describe('SpecReporter', function () {
 							});
 						});
 
-						it('should reset failedSpecs object', function () {
-							proclaim.deepEqual(newSpecReporter.failedSpecs, {});
-						});
-
-						it('should reset currentSuite arrays', function () {
-							proclaim.equal(newSpecReporter.currentSuite.length, 0);
-						});
-
-						it('should not add anything to the errors array', function () {
-							proclaim.equal(errors.length, 0);
+						it('should not write to the output a github annotation for each test failure', function () {
+							proclaim.notCalled(console.log);
 						});
 					});
 
@@ -98,7 +81,7 @@ describe('SpecReporter', function () {
 							failedSpec = {
 								suite: ['suite name'],
 								description: 'description of test',
-								log: ['log', 'test/add.test.js']
+								log: ['2+2 doest not equal carrot', 'test/add.test.js:1:12']
 							};
 							newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
 							newSpecReporter.currentSuite.push(failedSpec.suite);
@@ -110,83 +93,13 @@ describe('SpecReporter', function () {
 							});
 						});
 
-						it('should reset failedSpecs object', function () {
-							proclaim.deepEqual(newSpecReporter.failedSpecs, {});
+						it('should write to the output a github annotation for each test failure', function () {
+							proclaim.calledOnce(console.log);
+							proclaim.calledWithExactly(
+								console.log,
+								`::error file=test/add.test.js,line=1,col=12::suite name > description of test%0ATest Browser errored with: 2+2 doest not equal carrot`
+							);
 						});
-
-						it('should reset currentSuite arrays', function () {
-							proclaim.equal(newSpecReporter.currentSuite.length, 0);
-						});
-
-						it('should add error message to the errors array', function () {
-							proclaim.deepEqual(errors, [
-								colors.white('Test Browser failed specs:'),
-								colors.white('test/add.test.js') + ' suite name > description of test',
-								colors.white('Test Browser errored with:') + ' ' + colors.red('log'),
-								'1 tests failed across 1 browsers.'
-							]);
-						});
-
-						it('should be idempotent', () => {
-							newSpecReporter.currentSuite.push(failedSpec.suite);
-							newSpecReporter.failedSpecs = {
-								[browser.id]: [failedSpec]
-							};
-							newSpecReporter.onRunComplete([browser], {
-								failed: 1
-							});
-
-							proclaim.deepEqual(errors, [
-								colors.white('Test Browser failed specs:'),
-								colors.white('test/add.test.js') + ' suite name > description of test',
-								colors.white('Test Browser errored with:') + ' ' + colors.red('log'),
-								'1 tests failed across 1 browsers.'
-							]);
-						});
-					});
-				});
-			});
-
-			describe('onSpecComplete', function () {
-				let newSpecReporter;
-				let browser;
-				let failedResult;
-				let passedResult;
-
-				beforeEach(() => {
-					browser = {
-						name: 'Test Browser',
-						id: 'testValue'
-					};
-					failedResult = {
-						suite: ['suite name'],
-						description: 'description of test',
-						log: ['log message'],
-						success : false
-					};
-
-					passedResult = {
-						suite: ['suite name'],
-						description: 'description of test',
-						log: ['log message'],
-						success : true
-					};
-					newSpecReporter = new SpecReporter[1](baseReporterDecorator, formatError);
-				});
-
-				describe('no failed specs', () => {
-					it('should not anything to failedSpecs ', function () {
-						newSpecReporter.onSpecComplete(browser, passedResult);
-						proclaim.deepEqual(newSpecReporter.failedSpecs, {});
-					});
-				});
-
-				describe('failed specs', () => {
-					it('should add array of failed specs for browser to the failedSpecs property ', function () {
-						newSpecReporter.onSpecComplete(browser, failedResult);
-
-						const failedSpec = Object.create(null);
-						failedSpec[browser.id] = [failedResult];
 					});
 				});
 			});
@@ -230,6 +143,10 @@ describe('SpecReporter', function () {
 					it('should not add anything to the errors array', function () {
 						proclaim.equal(errors.length, 0);
 					});
+
+					it('should not write to the output a github annotation for each test failure', function () {
+						proclaim.notCalled(console.log);
+					});
 				});
 
 				describe('with browsers', function () {
@@ -253,6 +170,10 @@ describe('SpecReporter', function () {
 
 						it('should not add anything to the errors array', function () {
 							proclaim.equal(errors.length, 0);
+						});
+
+						it('should not write to the output a github annotation for each test failure', function () {
+							proclaim.notCalled(console.log);
 						});
 					});
 
@@ -314,6 +235,10 @@ describe('SpecReporter', function () {
 								colors.red('test/add.test.js\t'),
 								'1 tests failed across 1 browsers.'
 							]);
+						});
+
+						it('should not write to the output a github annotation for each test failure', function () {
+							proclaim.notCalled(console.log);
 						});
 					});
 				});
