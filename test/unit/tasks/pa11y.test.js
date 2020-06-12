dc6b902f04401691bc8fa807ebd068b5c226d18b
Jake Champion <me@jakechampion.name>
2020-06-12 17:33:05 +0100

    Add pa11y annotations test

--- test/unit/tasks/pa11y.test.js
+++ test/unit/tasks/pa11y.test.js
@@ -1,13 +1,17 @@
 /* eslint-env mocha */
 'use strict';
 
+const mockery = require('mockery');
 const proclaim = require('proclaim');
+const sinon = require('sinon');
+sinon.assert.expose(proclaim, {
+	includeFail: false,
+	prefix: ''
+});
+
 const process = require('process');
 const fs = require('fs-extra');
 const path = require('path');
-
-const demo = require('../../../lib/tasks/demo-build');
-const pa11y = require('../../../lib/tasks/pa11y');
 
 const obtPath = process.cwd();
 const oTestPath = 'test/unit/fixtures/o-test';
@@ -17,16 +21,35 @@ describe('Test task', function() {
 		const pathSuffix = '-test-pa11y';
 		const testTestPath = path.resolve(obtPath, oTestPath + pathSuffix);
 
+		let console;
+		let demo;
+		let pa11y;
 		beforeEach(function() {
+			mockery.enable({
+				useCleanCache: true,
+				warnOnReplace: false,
+				warnOnUnregistered: false
+			});
+			console = {
+				log: sinon.stub()
+			};
+			mockery.registerMock('is-ci', true);
+			process.env.CI = true;
+			global.console = console;
 			fs.copySync(path.resolve(obtPath, oTestPath), testTestPath);
 			process.chdir(testTestPath);
+			demo = require('../../../lib/tasks/demo-build');
+			pa11y = require('../../../lib/tasks/pa11y');
 			return demo({
 				demoConfig: 'origami.json',
 				demoFilter: ['pa11y']
 			});
 		});
 
-		afterEach(function() {
+		afterEach(() => {
+			mockery.resetCache();
+			mockery.deregisterAll();
+			mockery.disable();
 			process.chdir(obtPath);
 			fs.removeSync(testTestPath);
 		});
@@ -59,6 +82,19 @@ describe('Test task', function() {
 				return pa11y().task()
 					.catch(function (results) {
 						proclaim.isInstanceOf(results, Error);
+					});
+			});
+
+			it('should write to the output a github annotation for each test failure', function () {
+				this.timeout(30000);
+				return pa11y().task()
+					.catch(function (results) {
+						proclaim.isInstanceOf(results, Error);
+						proclaim.calledThrice(console.log);
+						proclaim.calledWithExactly(
+							console.log,
+							`::error file=demos/src/pa11y.mustache,line=1,col=1::%0A • Error: All page content must be contained by landmarks (https://dequeuniversity.com/rules/axe/3.5/region?application=axeAPI)%0A   ├── region%0A   ├── html > body > button:nth-child(1)%0A   └── <button style="background-color:blue; color:blue">No contrast</button>`
+						);
 					});
 			});
 		});
