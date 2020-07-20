8bb5f986b06b606ff80a34479673537ebfec8b74
Lee Moody <notlee@users.noreply.github.com>
2020-07-20 15:29:48 +0100

    Update test/integration/verify/fixtures/js-custom-eslint/main.js

--- test/integration/verify/fixtures/js-custom-eslint/main.js
+++ test/integration/verify/fixtures/js-custom-eslint/main.js
@@ -1,4 +1,4 @@
 // tab indent invalid with custom .eslintrcjs configuration
 export default {
- eg: 'This was indented with a tab'
+ eg: 'This was indented with a space, instead of the usual tab'
 };
