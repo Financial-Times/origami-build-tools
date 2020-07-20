26404a77ed463e07de1cef1f3c4cd9618e6aef7d
notlee <lee.moody@ft.com>
2020-07-20 15:26:09 +0100

    Allow components to define custom `.eslintrc.js` configuration.
    As per the Origami specification:
    https://github.com/Financial-Times/origami-website/blame/49e27d162ca28b212512e128d7987d9a58ee38b7/_specification-v1/javascript.md#L24

--- test/integration/verify/verify.test.js
+++ test/integration/verify/verify.test.js
@@ -60,6 +60,27 @@ describe('obt verify', function () {
 			});
 		});
 
+		describe('component with custom .eslintrc.js configuration', function () {
+
+			beforeEach(function () {
+				// Change the current working directory to the folder which contains the project we are testing against.
+				// We are doing this to replicate how obt is used when executed inside a terminal.
+				process.chdir(path.join(__dirname, '/fixtures/js-custom-eslint'));
+			});
+
+			afterEach(function () {
+				// Change the current working directory back to the directory where you started running these tests from.
+				process.chdir(process.cwd());
+			});
+
+			it('should not error given conformance with custom rules', function () {
+				return obtBinPath()
+					.then(obt => {
+						return execa(obt, ['verify']);
+					});
+			});
+		});
+
 		describe('component with valid ES5 js', function () {
 
 			beforeEach(function () {
