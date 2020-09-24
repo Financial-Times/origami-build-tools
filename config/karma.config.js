8bc577d3cd0885535e362c868ef122f54f0f585b
Jake Champion <me@jakechampion.name>
2020-09-24 15:58:59 +0100

    Add regenerator runtime to ensure async functions work when compiled to es5

--- config/karma.config.js
+++ config/karma.config.js
@@ -43,6 +43,7 @@ module.exports.getBaseKarmaConfig = function (opts = { ignoreBower: false }) {
 
 			// list of files / patterns to load in the browser
 			files: [
+				require.resolve('regenerator-runtime'),
 				'test/*.js',
 				'test/**/*.js',
 				'main.scss'
@@ -66,7 +67,7 @@ module.exports.getBaseKarmaConfig = function (opts = { ignoreBower: false }) {
 			// preprocess matching files before serving them to the browser
 			// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 			preprocessors: {
-				'test/**/*.js': ['scrumple', 'swc', 'sourcemap'],
+				'test/**/*.js': ['swc', 'scrumple', 'sourcemap'],
 				'main.scss': ['scss']
 			},
 			scssPreprocessor: {
