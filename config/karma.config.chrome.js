5eaecb5e17cebd3c37ff7f6d4b25ae0d48769bb4
Jake Champion <me@jakechampion.name>
2021-05-10 18:02:43 +0100

    Restore karma.config.chrome.js

--- config/karma.config.chrome.js
+++ config/karma.config.chrome.js
@@ -10,7 +10,7 @@ module.exports.getChromeKarmaConfig = function (opts = { sassIncludePaths: [] })
 			karmaBaseConfig,
 			{
 				browsers: ['ChromeHeadless'],
-				logLevel: constants.LOG_DEBUG
+				logLevel: constants.LOG_ERROR
 			}
 		);
 
